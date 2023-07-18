22954456815dc86b38efb1cade7ec4e0372501ab
994821710@qq.com <994821710@qq.com>
2023-07-18 23:59:33 +0800

    普通消息和事务消息

---
+++ src/main/java/space/lichuang/rocketmq/producer/RocketmqProducerApplication.java
@@ -0,0 +1,13 @@
+package space.lichuang.rocketmq.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProducerApplication.class, args);
+    }
+
+}
